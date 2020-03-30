c7cffce36941982bb2c833b9e3414f7f66c41000
sam <samreality4@gmail.com>
2020-03-30 16:23:22 -0700

    initial commit

---
+++ build/precache-manifest.bb87cf0991ebba828468e1384a536197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322cbec6926b7b8f6caa1ca70f2780b0",
+    "url": "/samgsite/index.html"
+  },
+  {
+    "revision": "547d31916ee0b7bececd",
+    "url": "/samgsite/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "547d31916ee0b7bececd",
+    "url": "/samgsite/static/js/2.0930b21c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/samgsite/static/js/2.0930b21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf5f27b90f3f3913681",
+    "url": "/samgsite/static/js/main.63ec5562.chunk.js"
+  },
+  {
+    "revision": "1603a87a139648a0981e",
+    "url": "/samgsite/static/js/runtime-main.d55b28fc.js"
+  }
+]);
